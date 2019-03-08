10807c912791dea66a1b7b8ac400949ae6fdb944
i4asia <i4asia@localhost.localdomain>
2019-03-08 14:29:01 +0800

    first commit

---
+++ database/migrations/2019_03_08_061442_create_children_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChildrenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('children', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('birthdate');
+            $table->string('level');
+            $table->string('batting');
+            $table->string('throwing_hard');
+            $table->string('special_medical_condition');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('children');
+    }
+}
