a28b6ccf3060a5c833dcde2809e6c7cc103344b8
i4asia <i4asia@localhost.localdomain>
2019-03-15 16:06:11 +0800

    button layout fix

--- resources/views/admin/client/events/index.blade.php
+++ resources/views/admin/client/events/index.blade.php
@@ -3,20 +3,20 @@
 <div class="container-fluid">
     <div class="card">
         <div class="card-body">
-            <div class="row">
-            <div class="col-lg-3"><h2 class="card-title">Event Management</h2></div>
-            <div class="col-lg-5">
-            <form class="form-inline" action="{{ route('event.search')}}" method="POST">
-                <div class="form-group">
-                    @csrf
-                    <input type="text" name="search" class="form-control mx-sm-3" style="width:600px" placeholder="Search an event...">
-                    <input type="submit" class="btn btn-primary" class="form-control" value="Search">
+            <div class="row pb-5">
+                <div class="col-sm"><h2 class="card-title">Event Management</h2></div>
+                <div class="col-sm">
+                    <form class="form-inline" action="{{ route('event.search')}}" method="POST">
+                        <div class="form-group">
+                            @csrf
+                            <input type="text" name="search" class="form-control mx-sm-3" style="" placeholder="Search an event...">
+                            <input type="submit" class="btn btn-primary" class="form-control" value="Search">
+                        </div>
+                    </form>
                 </div>
-            </form>
-            </div>
-            @can('isAdmin')
-            <div class="col-lg-1 offset-lg-3"><a href="{{ route('event.create')}}"  class="btn btn-sm btn-success float-right"><i class="fa fa-plus"></i> Create a New Event</a></div>
-            @endcan
+                @can('isAdmin')
+                <div class="col-sm"><a href="{{ route('event.create')}}"  class="btn btn-sm btn-success float-right"><i class="fa fa-plus"></i> Create a New Event</a></div>
+                @endcan
             </div>
             <table class="table">
                 
