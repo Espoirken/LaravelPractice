014cb3c2c78be72eda0a35373e9e968e2049182a
Roland Dugan <rdugan@i4asiacorp.com>
2019-03-18 14:09:24 +0800

    Add  value

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -164,7 +164,9 @@ class EventController extends Controller
         $events = Event::where('title', 'like', '%' . request('search') . '%')
         ->orWhere('detail', 'like', '%' . request('search') . '%')
         ->paginate(10);
-        return view('admin.client.events.index')->with('events', $events);
+        $now = Carbon::now('Asia/Manila');
+        return view('admin.client.events.index')->with('events', $events)
+                                                ->with('now', $now);
     }
 
     public function attend($id)
