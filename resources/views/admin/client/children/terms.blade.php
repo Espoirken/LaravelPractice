61c8748ff518fa5cc37fc8eb7b9f76497294c61e
Roland Dugan <rdugan@i4asiacorp.com>
2019-03-27 16:06:28 +0800

    Change terms/waiver details

--- resources/views/admin/client/children/terms.blade.php
+++ resources/views/admin/client/children/terms.blade.php
@@ -10,21 +10,25 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Terms and Condition</h5>
+        <h5 class="modal-title" id="exampleModalLongTitle">GENERAL WAIVER</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
       <div class="form-group">
-          <label for="terms">
-                  PLEASE READ THIS AGREEMENT CAREFULLY. By submitting your application and by your use of the Service, you agree to comply with all of the terms and conditions set out in this Agreement. Website.com may terminate your account at any time, with or without notice, for conduct that is in breach of this Agreement, for conduct that Website.com believes is harmful to its business, or for conduct where the use of the Service is harmful to any other party.
-                  
-                  Website.com may, in its sole discretion, change or modify this Agreement at any time, with or without notice. Such changes or modifications shall be made effective for all Subscribers upon posting of the modified Agreement to this web address. You are responsible to read this document from time to time to ensure that your use of the Service remains in compliance with this Agreement.</label>
+          <label for="terms">We, the undersigned, certify that we have allowed our child, whose name appears above, 
+          to join the April 2019 Shibata Shogo Baseball Clinic voluntarily and of our and his/her own free will. 
+          We shall not hold the organizers, as well as their representatives, employees, and staff, liable civilly, 
+          criminally nor administratively, or financially, should any injury, loss, impairment or any unfortunate incident 
+          happen to our child on the occasion of his voluntary participation in this clinic. We hereby release, relieve and exempt 
+          said parties from any and all responsibilities and liabilities. We, the undersigned, my child, and all other members of 
+          our family agree to be bound by the rules, regulations, schedules, and guidelines of Shibata Shogo Baseball Class.</label>
           <input type="text" name="terms" class="form-control" value="I Agree" hidden>
       </div>
       <div class="form-group">
-          <label for="terms">You must type "I Agree" to continue</label>
+          <label for="terms">Please read the waiver above.</label><br>
+          <label>You must type "I Agree" to continue</label>
           <input id="terms" type="text" class="form-control" name="terms_confirmation">
       </div>
       </div>
